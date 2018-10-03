cca736e249da025a072c25caad32ea8ef6e79706
Charles Woodring <charles.s.woodring@gmail.com>
2018-10-02 20:06:39 -0500

    add json file

--- javascripts/components/characters.js
+++ javascripts/components/characters.js
@@ -2,18 +2,13 @@ import { printToDom } from "../helpers/util.js";
 import {detailsBuilder} from "./detail.js";
 
 const characters =[
-    {id:"character1", name: "Jon Snow", house: "Stark", imageURL: "https://vignette.wikia.nocookie.net/gameofthrones/images/a/a5/Profile-JonSnow-707.png/revision/latest?cb=20170828030553"},
-    {id:"character2", name: "Daenerys Targaryen", house: "Targaryen", imageURL: "https://cdn1.ntv.com.tr/gorsel/yasam/emilia-clarke-daenerys-targaryen-imajini-degistirdi/,dJTaLetJRk6bPVKhZm3xrA.jpg?w=960&mode=max&v=20170920100213652"},
-    {id:"character3", name: "Cersei Lannister", house: "Lannister", imageURL: "https://upload.wikimedia.org/wikipedia/en/9/94/Cersei_Lannister-Lena_Headey.jpg"},
-    {id:"character4", name: "Tyrion Lannister", house: "Lannister", imageURL: "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg"}
-
 ];
 
 const characterClick = (e) => {
     const characterId = e.target.closest(".character-card").id;
     const currentCharacter = characters.find(x => characterId === x.id);
     detailsBuilder(currentCharacter);
-    console.log(currentCharacter)
+    // console.log(currentCharacter)
 }
 
 
--- javascripts/components/detail.js
+++ javascripts/components/detail.js
@@ -1,20 +1,32 @@
 import {printToDom} from "../helpers/util.js";
+import {charactersBuilder} from "./characters.js";
+
+const closeButtonEvent = () => {
+    const closeButton = document.getElementById("close");
+closeButton.addEventListener("click", charactersBuilder);
+};
 
 const detailsBuilder = (character) => {
     console.log("character inside details", character);
     let domString = "";
     domString += `<div class="col-6 offset-md-3">`;
     domString +=    `<div class="row">`;
-    domString +=    `<div class="col">`;
+    domString += `<button class="btn btn-danger" id="close">x</button>`;
+    domString +=    `</div>`;
+    domString +=   `<div class="row">`;
+    domString +=     `<div class="col">`;
     domString +=        `<img src="${character.imageURL}" alt"${character.name}"/>`;
     domString +=    `</div>`;
     domString +=    `<div class="col">`;
     domString +=        `<h1>${character.name}</h1>`;
     domString +=        `<h3>${character.house}</h3>`;
     domString +=    `</div>`;
-    domString +=    `</div>`;
+    domString +=   `</div>`;
     domString += `</div>`;
     printToDom (domString);
+    closeButtonEvent ();
+
 };
+
 
 export {detailsBuilder};
